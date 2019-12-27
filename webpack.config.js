e3e4ad0371bc65ee6e022aaf23a37c8fd0869ef8
Green Wang <cocowool@gmail.com>
2019-12-27 19:45:07 +0800

    add webpack support

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: "development",
+    entry: ['./source/js/index.js'],
+    // module: {
+    //     rules: [
+    //         { test: /\.scss$/, use: [{
+    //             loader: MiniCssExtractPlugin.loader,
+    //             options: {
+    //                 publicPath: '../dist'
+    //             }
+    //         }, 'css-loader', 'sass-loader']}
+    //     ]
+    // },
+    plugins: [
+        // new MiniCssExtractPlugin({
+        //     // 类似 webpackOptions.output里面的配置 可以忽略
+        //     filename: '[name].css',
+        //     chunkFilename: '[id].css',
+        //   }),
+        new HtmlWebpackPlugin({
+            title: 'vMeeting Project'
+            // template: './src/index.html'
+        })
+    ]
+}
